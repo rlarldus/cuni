a8c8dd47c059c0ca3d4fbf404b267454d35316b4
rlarldus <rlarldus4765@naver.com>
2020-12-07 21:19:36 +0900

    게시물 리스팅

---
+++ src/main/java/com/example/sbs/cuni/dto/Article.java
@@ -0,0 +1,15 @@
+package com.example.sbs.cuni.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Article {
+	private int id;
+	private String regDate;
+	private String title;
+	private String body;
+}
